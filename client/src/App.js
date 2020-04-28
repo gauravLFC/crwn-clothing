d4c39b0687f0ecfc54b8880c0cc7c48b4e66c22e
Gaurav Sharma <gauravlfc.93@gmail.com>
2020-04-28 10:17:57 +0530

    Added media queries for mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.relectors';
 import { checkUserSession } from './redux/user/user.action';
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -20,6 +20,7 @@ const App = ({ checkUserSession, selectCurrentUser }) => {
   }, [checkUserSession]);
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
